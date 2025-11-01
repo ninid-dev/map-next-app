d37f2b559ae14e121fbaaa03d93f280c6cbad91b
Danish <ninidwork@gmail.com>
2025-11-01 19:33:52 +0700

    refactor(constant): ปรับให้เป็นค่า label, value แทน

--- constants/index.js
+++ constants/index.js
@@ -1,7 +1,31 @@
+// export const TITLE_REQUEST_LIST = [
+//   'น้ำท่วม',
+//   'ไฟไหม้',
+//   'อุบัติเหตุ',
+//   'ขอของยังชีพ',
+//   'ผู้ป่วยฉุกเฉิน',
+// ]
+
 export const TITLE_REQUEST_LIST = [
-  'น้ำท่วม',
-  'ไฟไหม้',
-  'อุบัติเหตุ',
-  'ขอของยังชีพ',
-  'ผู้ป่วยฉุกเฉิน',
+  {
+    label: 'น้ำท่วม',
+    value: 'flood'
+  },
+  {
+    label: 'ไฟไหม้',
+    value: 'fire'
+  },
+  {
+    label: 'อุบัติเหตุ',
+    value: 'accident'
+  },
+  {
+    label: 'ขอของยังชีพ',
+    value: 'relief_request'
+  },
+  {
+    label: 'ผู้ป่วยฉุกเฉิน',
+    value: 'emergency_patient'
+  },
 ]
+
