7e9e6182e67b9b7c71205dda1b3f4b205c48e8df
Danish <ninidwork@gmail.com>
2025-11-01 19:35:04 +0700

    feat(map-view): เพิ่ม map marker icon

--- components/MapView.jsx
+++ components/MapView.jsx
@@ -4,15 +4,75 @@ import "leaflet/dist/leaflet.css";
 
 import { icon } from 'leaflet'
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import {useEffect, useMemo, useRef, useState} from "react";
+import {isEmpty} from "lodash";
 
-const ICON = icon({
-  iconUrl: "assets/marker.png",
-  iconSize: [32, 32],
-})
+function DraggableMarker(props) {
+  const { center, onSendData } = props;
+  const [position, setPosition] = useState(center)
+  const markerRef = useRef(null)
+
+  const ICON = icon({
+    iconUrl: "/assets/marker.png",
+    iconSize: [32, 32],
+  })
+
+  const eventHandlers = useMemo(
+    () => ({
+      dragend() {
+        const marker = markerRef.current
+        if (marker != null) {
+          const cord = marker.getLatLng()
+          setPosition([cord.lat.toFixed(6), cord.lng.toFixed(6)])
+        }
+      },
+    }),
+    [],
+  )
+
+  const getAddress = async () => {
+    try {
+      const lat = position[0]
+      const lng = position[1]
+      // TODO: เอาขึ้น prd ไม่ได้ ติด policy ตอนนี้แค่ทดลอง
+      const url = `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lng}&format=json&accept-language=th`
+
+      const res = await fetch(url)
+      const data = await res.json()
+
+      const address = data.display_name || 'ไม่พบข้อมูล'
+
+      onSendData({
+        address,
+        position: {
+          lat,
+          lng
+        }
+      })
+    } catch (error) {
+      console.error('error get address', error)
+    }
+  }
+
+  useEffect(() => {
+    getAddress()
+    console.log('position', position)
+  }, [position])
+
+  return (
+    <Marker
+      draggable={true}
+      eventHandlers={eventHandlers}
+      position={position}
+      ref={markerRef}
+      icon={ICON}
+    />
+  )
+}
 
 export default function Map(props) {
-  const { position = [51.505, -0.09], zoom = 11 } = props
-    // [51.505, -0.09]
+  const { position = [], zoom = 16, onSendData } = props
+
   return (
     <MapContainer
       preferCanvas={true}
@@ -25,12 +85,7 @@ export default function Map(props) {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker icon={ICON} position={position}>
-        <Popup>
-          This Marker icon is displayed correctly with{" "}
-          <i>leaflet-defaulticon-compatibility</i>.
-        </Popup>
-      </Marker>
+      { !isEmpty(position) && <DraggableMarker center={position} onSendData={onSendData} />}
     </MapContainer>
   );
 }
